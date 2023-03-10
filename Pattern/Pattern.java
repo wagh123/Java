4a254f225c7c31c488c37b667de43799e60feca3
yogita <waghyogita@gmail.com>
2023-03-10 23:43:21 +0530

    Pattern Program

--- Pattern/Pattern.java
+++ Pattern/Pattern.java
@@ -11,16 +11,31 @@ public class Pattern {
 
     public static void floydsTriangle(int n){
         int counter = 1;
-        for(int i =1; i<=n; i++){
+        for(int i = 1; i<=n; i++){
             for(int j = 1; j<=i; j++){
-                System.out.print(counter + " ");
+                System.out.print(counter+" ");
                 counter++;
             }
             System.out.println();
         }
     }
+
+    public static void triangle0_1(int n){
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                if((i+j) % 2 == 0){
+                    System.out.print(1);
+                }else{
+                    System.out.print(0);
+                }
+            }
+            System.out.println();
+
+        }
+    }
     public static void main(String[] args) {
         Inverted_half_Pyramid_with_numbers(5);
         floydsTriangle(5);
+        triangle0_1(5);
     }
 }
