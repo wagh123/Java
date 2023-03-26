e24b041092a87fffc891589f7b7429852f928b95
yogita <waghyogita@gmail.com>
2023-03-26 15:43:46 +0530

    Spiral Matrix

--- 2D_Arrays/Creation.java
+++ 2D_Arrays/Creation.java
@@ -1,5 +1,29 @@
 import java.util.*;
 public class Creation {
+
+    public static void smallestAndLargest(int matrix[][]){
+        int minNum = Integer.MAX_VALUE;
+        int maxNum = Integer.MIN_VALUE;
+        for(int i = 0; i < matrix.length; i++){
+            for(int j =0; j<matrix[0].length; j++){
+                minNum = Math.min(minNum, matrix[i][j]);
+                maxNum = Math.max(maxNum, matrix[i][j]);
+            }
+        }
+        System.out.println(minNum+ " "+maxNum);
+    }
+    public static boolean search(int matrix[][], int key){
+        for(int i =0; i < matrix.length; i++){
+            for(int j = 0; j <matrix[0].length; j++){
+                if(matrix[i][j] == key){
+                    System.out.println("found at cell (" +i+","+j+")");
+                    return true;
+                }
+            }
+        }
+        System.out.println("key not found");
+        return false;
+    }
     public static void main(String args[]){
         int matrix[][] = new int[3][3];
         int n = matrix.length, m = matrix[0].length;
@@ -16,5 +40,7 @@ public class Creation {
            System.out.println();
 
         }
+        search(matrix, 5);
+        smallestAndLargest(matrix);
     }  
 }
